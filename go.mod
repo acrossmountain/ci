312ff54c59394abd2de0152afebf15c7d246da78
Qyq <18339795172@163.com>
2021-07-23 15:12:20 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/acrossmountain/ci
+
+go 1.15
+
+require (
+	github.com/go-spring/spring-boot v1.0.5
+	github.com/go-spring/spring-web v1.0.5
+	github.com/go-spring/starter-gin v1.0.5
+)
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	SpringBoot "github.com/go-spring/spring-boot"
+
+	SpringWeb "github.com/go-spring/spring-web"
+
+	_ "github.com/go-spring/starter-gin"
+)
+
+func main() {
+	SpringBoot.GetMapping("/", func(ctx SpringWeb.WebContext) {
+		ctx.String(http.StatusOK, "hello world")
+
+	})
+
+	SpringBoot.RunApplication()
+}
